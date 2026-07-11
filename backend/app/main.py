b91e64eb3a469fd16c5bfe0aba62e05ca257acee
Boran <non.mrbora@gmail.com>
2026-07-11 12:39:07 +0300

    chore: remove backend code comments

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from app.routers import topics, learning_logs # Az önce oluşturduğumuz dosyayı çağırıyoruz
+from app.routers import topics, learning_logs
 
 app = FastAPI()     
 
-# Topics router'ını ana uygulamaya dahil (include) ediyoruz
 app.include_router(topics.router)
 app.include_router(learning_logs.router)
-# Kök endpoint'imiz aynen kalıyor
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
