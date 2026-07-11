16886f342a2bc35ea5dfc4251200f411adf6a649
Boran <non.mrbora@gmail.com>
2026-07-11 16:07:58 +0300

    feat: add users create endpoint

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from pydantic import BaseModel
 from sqlalchemy import text
 from app.database import engine
 
@@ -6,6 +7,10 @@ router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
 
 @router.get("")
 def get_users():
@@ -15,3 +20,20 @@ def get_users():
         )
         users = result.mappings().all()
         return [dict(user) for user in users]
+
+@router.post("", status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate):
+    with engine.begin() as connection:
+        result = connection.execute(
+            text("""
+                INSERT INTO users (username, email)
+                VALUES (:username, :email)
+                RETURNING *
+            """),
+            {
+                "username": user.username,
+                "email": user.email,
+            }
+        )
+        created_user = result.mappings().first()
+        return dict(created_user)
